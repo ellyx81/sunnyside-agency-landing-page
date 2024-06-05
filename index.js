1c2ed32c82b4cd4060b352a5bc90a36edc9aac91
Giselle Santos <gisellesan0801@gmail.com>
2024-06-05 16:02:27 +0800

    added remaining sections - added closeNavOnClick function; - added AOS - deleted extra file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
+var nav = document.querySelector('.navbar-collapse');
+
 function updateBodyOverflow() {
-    var nav = document.querySelector('.navbar-collapse');
     var body = document.body;
     var windowWidth = window.innerWidth;
     
@@ -14,7 +16,28 @@ function updateBodyOverflow() {
     }
 }
 
+
+function closeNavOnClick() {
+    var anchors = document.querySelectorAll('.navbar-expand-md .nav-link');
+    
+    anchors.forEach(function (anchor) {
+        anchor.addEventListener('click', function () {
+            nav.classList.remove('show');
+            updateBodyOverflow();
+        });
+    });
+}
+
 updateBodyOverflow();
+closeNavOnClick();
+
+AOS.init({
+    startEvent: 'DOMContentLoaded',
+    disable: function disable() {
+      var maxWidth = 768;
+      return window.innerWidth < maxWidth;
+    }
+});
 
 document.querySelector('.navbar-collapse').addEventListener('shown.bs.collapse', updateBodyOverflow);
 document.querySelector('.navbar-collapse').addEventListener('hidden.bs.collapse', updateBodyOverflow);
